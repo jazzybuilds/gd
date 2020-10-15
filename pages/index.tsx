27f31cdf7f7ce2ccc43f06b7ee1c0799e7e6d8af
Alex Shyba <alexs@uniform.dev>
2020-10-15 07:57:27 -0700

    enabling start/dev

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,21 +1,19 @@
-import React from "react";
+import React from 'react';
 
 // Uniform
-import {
-  BasePlaceholder,
-  PageComponent,
-  UniformContext,
-  NextPageProps,
-  getNextPageProps,
-  createConsoleLogger,
-} from "@uniformdev/next";
+import { BasePlaceholder, PageComponent, UniformContext, NextPageProps, getNextPageProps, createConsoleLogger, } from '@uniformdev/next';
+import { UniformContextProps } from '@uniformdev/common-client';
+
+import uniformConfig from '../uniform.config';
+const context: UniformContextProps = uniformConfig();
+context.logger = context.logger || createConsoleLogger();
 
 // Components Index
 const componentsIndex: any = {};
 
 class Placeholder extends BasePlaceholder {
   constructor(props) {
-    super(props, componentsIndex, createConsoleLogger(), {});
+    super(props, componentsIndex, createConsoleLogger(), context.options);
   }
 }
 
@@ -23,22 +21,19 @@ componentsIndex.Placeholder = Placeholder;
 
 // Page
 export default class extends React.Component<NextPageProps> {
-  static async getInitialProps(arg: any) {
-    return await getNextPageProps(arg);
-  }
+    static async getInitialProps(arg: any) {
+        return await getNextPageProps(arg);
+    }
 
-  render() {
-    return (
-      <UniformContext.Provider value={{ logger: createConsoleLogger() }}>
-        <PageComponent {...this.props} components={componentsIndex}>
-          {(renderingContext) => (
-            <Placeholder
-              placeholderKey="/"
-              renderingContext={renderingContext}
-            />
-          )}
-        </PageComponent>
-      </UniformContext.Provider>
-    );
-  }
+    render() {
+        return (
+            <UniformContext.Provider value={context}>
+                <PageComponent {...this.props} components={componentsIndex}>
+                    {(renderingContext) => (
+                        <Placeholder placeholderKey="/" renderingContext={renderingContext} />
+                    )}
+                </PageComponent>
+            </UniformContext.Provider>
+        );
+    }
 }
