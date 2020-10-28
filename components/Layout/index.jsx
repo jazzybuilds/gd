fa9d905e0ea45995fe2c833f776ea290c684682c
Marfat Abdullahi <marfat.abdullahi@akqa.com>
2020-10-28 14:09:00 +0000

    Loading fonts locally

--- components/Layout/index.jsx
+++ components/Layout/index.jsx
@@ -9,10 +9,6 @@ const MVCLayout = (props) => {
     <>
       <Head>
         <link href="/main.css" rel="stylesheet" />
-        {/* <link href="https://guidedogspoc.ngrok.io/-/media/Feature/Experience-Accelerator/Foundation/Foundation/Styles/optimized-min.css?t=20190703T145413Z" rel="stylesheet" /> */}
-        {/* <link href="https://guidedogspoc.ngrok.io/-/media/Themes/GuideDogs/GuideDogsDotOrg/GuideDogsTheme/styles/optimized-min.css?t=20200914T150421Z" rel="stylesheet" />     */}
-        {/* <link href="https://guidedogspoc.ngrok.io/-/media/Base-Themes/Core-Libraries/styles/optimized-min.css?t=20190703T145418Z" rel="stylesheet" /> */}
-        <link href="https://guidedogspoc.ngrok.io/-/media/Base-Themes/Main-Theme/styles/optimized-min.css?t=20190703T145420Z" rel="stylesheet" />
 
         {/* 
         <link
