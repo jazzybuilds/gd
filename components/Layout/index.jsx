f114e51d04480b66564fd2dc8d41edea33424e1b
Marfat Abdullahi <marfat.abdullahi@akqa.com>
2020-10-29 11:17:34 +0000

    Meta added

--- components/Layout/index.jsx
+++ components/Layout/index.jsx
@@ -9,6 +9,15 @@ const MVCLayout = (props) => {
     <>
       <Head>
         <title>Guide Dogs UK Charity For The Blind And Partially Sighted</title>
+        <meta content="Guide Dogs: Providing mobility for the blind and partially sighted. Supporting research, raising awareness and campaigning for the visually impaired. We will not rest until people who are blind or partially sighted can enjoy the same freedom of movement as everyone else." name="description"  />
+
+        <meta content="Guide Dogs: Providing mobility for the blind and partially sighted. Supporting research, raising awareness and campaigning for the visually impaired"  property="og:description" />
+        <meta content="128733004236479"  property="fb:app_id" />
+        <meta content="website"  property="og:type" />
+        <meta content="Guide Dogs Site"  property="og:site_name" />
+        <meta content="https://www.guidedogs.org.uk:443/-/media/Project/GuideDogs/GuideDogsDotOrg/Images/opengraph/homepage/Black-labrador-looks-up-at-camera.jpg"  property="og:image" />
+        <meta content="Guide Dogs UK"  property="og:title" />
+        <meta content="https://www.guidedogs.org.uk/"  property="og:url" />
         <link href="/main.css" rel="stylesheet" />
 
 {/*         
