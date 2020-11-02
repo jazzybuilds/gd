3e0b54a611badff3721b03575983a56b55814ea3
Kateryna Butenko <kb@altola.co>
2020-11-02 22:41:44 +0100

    fix: going back to standard Next document
    Custom document prevented React from working

--- components/Layout/index.jsx
+++ components/Layout/index.jsx
@@ -1,4 +1,3 @@
-import Head from "next/head";
 import UniformPlaceholder from "../UniformPlaceholder";
 
 const MVCLayout = (props) => {
@@ -7,44 +6,6 @@ const MVCLayout = (props) => {
 
   return (
     <>
-      <Head>
-        <title>Guide Dogs UK Charity For The Blind And Partially Sighted</title>
-        <meta content="Guide Dogs: Providing mobility for the blind and partially sighted. Supporting research, raising awareness and campaigning for the visually impaired. We will not rest until people who are blind or partially sighted can enjoy the same freedom of movement as everyone else." name="description"  />
-
-        <meta content="Guide Dogs: Providing mobility for the blind and partially sighted. Supporting research, raising awareness and campaigning for the visually impaired"  property="og:description" />
-        <meta content="128733004236479"  property="fb:app_id" />
-        <meta content="website"  property="og:type" />
-        <meta content="Guide Dogs Site"  property="og:site_name" />
-        <meta content="https://www.guidedogs.org.uk:443/-/media/Project/GuideDogs/GuideDogsDotOrg/Images/opengraph/homepage/Black-labrador-looks-up-at-camera.jpg"  property="og:image" />
-        <meta content="Guide Dogs UK"  property="og:title" />
-        <meta content="https://www.guidedogs.org.uk/"  property="og:url" />
-        <link href="/main.css" rel="stylesheet" />
-{/*         
-        <link
-          href="https://gdogskb.blob.core.windows.net/$web/-/media/Feature/Experience-Accelerator/Foundation/Foundation/Styles/optimized-min.css?t=20190703T145413Z"
-          rel="stylesheet"
-        />
-        <link
-          href="https://gdogskb.blob.core.windows.net/$web/-/media/Base-Themes/Core-Libraries/styles/optimized-min.css?t=20190703T145418Z"
-          rel="stylesheet"
-        />
-        <link
-          href="https://gdogskb.blob.core.windows.net/$web/-/media/Base-Themes/Main-Theme/styles/optimized-min.css?t=20190703T145420Z"
-          rel="stylesheet"
-        />
-        <link
-          href="https://gdogskb.blob.core.windows.net/$web/-/media/Themes/GuideDogs/GuideDogsDotOrg/GuideDogsTheme/styles/optimized-min.css?t=20200914T150421Z"
-          rel="stylesheet"
-        /> */}
-
-      
-        {/* <UniformPlaceholder
-          placeholderKey="/head"
-          placeholderComponent={placeholderComponent}
-          {...props}
-        ></UniformPlaceholder> */}
-      </Head>
-
       <UniformPlaceholder
         placeholderKey="/body-top"
         placeholderComponent={placeholderComponent}
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,24 +1,45 @@
-import React from 'react';
-import Document, { Main } from 'next/document';
-import CustomHead from '../components/CustomHead';
+import React from "react";
+import Document, { Html, Head, Main, NextScript } from "next/document";
+//import CustomHead from '../components/CustomHead';
 
 // When Sitecore solution does not have personalization rules and when it does not require SPA-navigation
 // it makes sense to disable all nextjs scripts to minimize javascript bundle and fit performance budget.
 
 // CustomDocument replaces stock.js Document component to replace stock Head component
 export default class CustomDocument extends Document {
-    render() {
-        return (
-            <html lang="en">
-                {/* CustomHead replaces stock Next.js Head component to disable prefetching links */}
-                <CustomHead />
-                <body className="default-device">
-                    <Main />
-
-                    <script src="/scripts.js"></script>
-                    <script src="/maps.js"></script>
-                </body>
-            </html>
-        );
-    }
+  render() {
+    return (
+      <Html>
+        <Head>
+          <title>
+            Guide Dogs UK Charity For The Blind And Partially Sighted
+          </title>
+          <meta
+            content="Guide Dogs: Providing mobility for the blind and partially sighted. Supporting research, raising awareness and campaigning for the visually impaired. We will not rest until people who are blind or partially sighted can enjoy the same freedom of movement as everyone else."
+            name="description"
+          />
+          <meta
+            content="Guide Dogs: Providing mobility for the blind and partially sighted. Supporting research, raising awareness and campaigning for the visually impaired"
+            property="og:description"
+          />
+          <meta content="128733004236479" property="fb:app_id" />
+          <meta content="website" property="og:type" />
+          <meta content="Guide Dogs Site" property="og:site_name" />
+          <meta
+            content="https://www.guidedogs.org.uk:443/-/media/Project/GuideDogs/GuideDogsDotOrg/Images/opengraph/homepage/Black-labrador-looks-up-at-camera.jpg"
+            property="og:image"
+          />
+          <meta content="Guide Dogs UK" property="og:title" />
+          <meta content="https://www.guidedogs.org.uk/" property="og:url" />
+          <link href="/main.css" rel="stylesheet" />
+        </Head>
+        <body className="default-device">
+          <Main />
+          <NextScript />
+          <script src="/scripts.js"></script>
+          <script src="/maps.js"></script>
+        </body>
+      </Html>
+    );
+  }
 }
