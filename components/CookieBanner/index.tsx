a73d079ce161306b5941abe2f0f52715dfb04e7a
Marfat Abdullahi <marfat.abdullahi@akqa.com>
2020-11-03 10:39:12 +0000

    Minor tidy up of cookie banner

--- components/CookieBanner/index.tsx
+++ components/CookieBanner/index.tsx
@@ -9,14 +9,17 @@ export const CookieBanner = (props) => {
     if (props.renderingContext.item.url.includes('privacy-policy') || document.cookie.includes(cookieName)) {
       setHideBanner(true)
     }
-  },[])
+  }, [])
 
   return (
-    <div className="privacy-warning permisive" style={{
-      display: hideBanner ? 'none' :'block'
-    }}>
+    <div 
+      className="privacy-warning permisive"
+      style={{
+        display: hideBanner ? 'none' : 'block'
+      }}
+    >
       <div className="info">
-        <p><span>We use necessary cookies to make our site work and give you the best possible experience. You can choose not to allow some types of cookies but this might affect how the website works. We would like to set optional analytics and advertising cookies to help us improve and customise your browsing experience; for analytics and metrics; and to allow Guide Dogs and our advertising partners to provide you with messages tailored to your interests.  We won’t set optional cookies unless you enable them.  For more information about the cookies we use, please see our <a href="/privacy-policy">Privacy Policy</a>. </span></p>
+        <p>We use necessary cookies to make our site work and give you the best possible experience. You can choose not to allow some types of cookies but this might affect how the website works. We would like to set optional analytics and advertising cookies to help us improve and customise your browsing experience; for analytics and metrics; and to allow Guide Dogs and our advertising partners to provide you with messages tailored to your interests.  We won’t set optional cookies unless you enable them.  For more information about the cookies we use, please see our <a href="/privacy-policy">Privacy Policy</a>.</p>
       </div>
       <div className="controls">
         <div className="submit">
@@ -25,11 +28,11 @@ export const CookieBanner = (props) => {
               document.cookie = `${cookieName}=accepted`;
               setHideBanner(true)
             }}
-            aria-label="Accept use of cookies">Accept</button>
+          >Accept</button>
         </div>
 
         <div className="submit" style={{ paddingLeft: '15px' }}>
-          <button onClick={() =>window.location.replace('privacy-policy#cookie-consent')}>Manage preferences</button>
+          <button onClick={() => window.location.replace('privacy-policy#cookie-consent')}>Manage preferences</button>
         </div>
 
         <div className="submit REJECT CLASS" style={{ paddingLeft: '15px' }}>
@@ -38,7 +41,7 @@ export const CookieBanner = (props) => {
               document.cookie = `${cookieName}=rejected`;
               setHideBanner(true)
             }}
-            aria-label="Reject use of cookies">Reject</button>
+          >Reject</button>
         </div>
       </div>
     </div>
