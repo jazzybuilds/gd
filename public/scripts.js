e5211d1df863363be5ae730cf51ca2adbfb002f9
Marfat Abdullahi <marfat.abdullahi@akqa.com>
2020-12-15 17:24:32 +0000

    By default not showing cookie banner

--- public/scripts.js
+++ public/scripts.js
@@ -1,7 +1,6 @@
 jQuery.noConflict()
 jQuery("a.js-responsiveNav-last.visuallyhidden").remove();
 
-jQuery(".privacy-warning").css("display", "none");
 if (!document.cookie.includes("privacy-notification")) {
   jQuery(".privacy-warning").css("display", "block");
 }
