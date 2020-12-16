590a6c9bc3b8d7bb10848db0c2b113b631e48041
Michael Carlisle <Michael.Carlisle@akqa.com>
2020-12-16 20:39:07 +0000

    Refactor redireects to use map service.

--- generate-redirects.js
+++ generate-redirects.js
@@ -11,7 +11,7 @@ function escapeRegex(string) {
 const { UNIFORM_API_URL } = process.env;
 
 function cleanFromUrl(url) {
-  url = url.trim().replace(/^\^/ig, '').replace(/^\//ig, '').replace(/\\\./ig, '.').replace(/\/$/ig, '')
+  url = url.trim().replace(/^\^/ig, '').replace(/^\//ig, '').replace(/\\\./ig, '.').replace(/\/$/ig, '');
   if (url.match(escapeRegex('(/)?$'))) {
     return `/${url.replace('(/)?$', '')}`.trim().replace(/ /gi, '%20');
   } else if (url.match(escapeRegex('$'))) {
@@ -21,15 +21,15 @@ function cleanFromUrl(url) {
 }
 
 function cleanToUrl(url) {
-  const toUrlParse = url.trim().replace('https://{HTTP_HOST}', '').replace('www.guidedogs.org.uk/', '/').trim().replace(/\/$/gi, '').replace(/^\//gi, '');;
-  const toUrl = (toUrlParse ? toUrlParse : '/').replace(/ /gi, '%20')
-  return toUrl.startsWith('http') ? toUrl : `/${toUrl}`
+  const toUrlParse = url.trim().replace('https://{HTTP_HOST}', '').replace('www.guidedogs.org.uk/', '/').trim().replace(/\/$/gi, '').replace(/^\//gi, '');
+  const toUrl = (toUrlParse ? toUrlParse : '/').replace(/ /gi, '%20');
+  return toUrl.startsWith('http') ? toUrl : `/${toUrl}`;
 }
 
 function parseLegacyRedirects(path) {
   const data = fs.readFileSync(path);
   const json = JSON.parse(parser.toJson(data));
-  const cleanRules = []
+  const cleanRules = [];
   json.rewrite.rules.rule.map(rule => {
     if (rule.action.redirectType !== '301') {
       throw new Error(`${rule.action.redirectType} not supported`)
@@ -37,43 +37,27 @@ function parseLegacyRedirects(path) {
     const fromUrl = cleanFromUrl(rule.match.url);
     const toUrl = cleanToUrl(rule.action.url);
     if (!cleanRules[fromUrl]) {
-      cleanRules[fromUrl] = toUrl
+      cleanRules[fromUrl] = toUrl;
     }
   })
   const paths = Object.keys(cleanRules).map(fromUrl => {
-    return `${fromUrl} ${cleanRules[fromUrl]} 301`
+    return `${fromUrl} ${cleanRules[fromUrl]} 301`;
   })
 
-  return `# Legacy Redirects\r\n${paths.join('\r\n')}`
-}
-
-function recurseRedirectData(data, paths) {
-  Object.keys(data).map(key => {
-    const item = data[key]
-    if (item.fields && item.fields.urlmapping) {
-      const rules = qs.parse(item.fields.urlmapping);
-      Object.keys(rules).map(rule => {
-        if (rule) {
-          const fromUrl = cleanFromUrl(rule);
-          const toUrl = cleanToUrl(rules[rule]);
-          paths.push(`${fromUrl} ${toUrl} 301`);
-        }
-      })
-    }
-    if (item.children) {
-      recurseRedirectData(item.children, paths)
-    }
-  })
+  return `# Legacy Redirects\r\n${paths.join('\r\n').toLowerCase()}`
 }
 
 function parseManagedRedirectData(data) {
-  const paths = [];
-  recurseRedirectData(data.children, paths)
-  return `# Managed Redirects\r\n${paths.join('\r\n')}`
+  const paths = data.redirects.map(item => {
+    const fromUrl = cleanFromUrl(item.source);
+    const toUrl = cleanToUrl(item.target);
+    return `${fromUrl} ${toUrl} 301`;
+  })
+  return `# Managed Redirects\r\n${paths.join('\r\n').toLowerCase()}`;
 }
 
 async function parseManagedRedirects() {
-  return fetch(`${UNIFORM_API_URL}/uniform/api/content/guidedogsdotorg/item/449fd60e-eea7-45c8-86c7-508ecb799e1d.json`)
+  return fetch(`${UNIFORM_API_URL}/uniform/api/content/guidedogsdotorg/map.json`)
     .then(res => res.json())
     .then(data => parseManagedRedirectData(data))
     .then(data => {
