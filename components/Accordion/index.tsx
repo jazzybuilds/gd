179613fd48e1d6bcba3448a1ab1968898cc80477
Marfat Abdullahi <marfat.abdullahi@akqa.com>
2020-12-19 10:41:55 +0000

    Replacing accordion content with video component when applicable

--- components/Accordion/index.tsx
+++ components/Accordion/index.tsx
@@ -1,6 +1,26 @@
 import React from 'react'
 import "../../styles/component-accordion.scss";
+import { Video } from '../Video';
 
+
+const Wrapper: React.FC<{ index: number, heading: string }> = (props) => {
+  return (
+    <li className="item" key={`accordion-${props.index}`}>
+      <button type="button" className="toggle-header js-accordionToggle" aria-expanded="false" aria-controls={`section${props.index}`} id={`toggle${props.index}`} data-tabindex={props.index}>
+        <span className="label">
+          <div className="component content small-12 columns">
+            <div className="component-content">
+              <div className="field-heading" dangerouslySetInnerHTML={{ __html: props.heading }} />
+            </div>
+          </div>
+        </span>
+      </button>
+      <div className="toggle-content js-accordionContent" id={`section${props.index}`} aria-labelledby={`toggle${props.index}`} aria-hidden="false">
+        {props.children}
+      </div>
+    </li>
+  )
+}
 export const Accordion = (props) => {
   const { renderingContext } = props
   const { item: { children } } = renderingContext
@@ -14,26 +34,31 @@ export const Accordion = (props) => {
       <div className="component-content">
         <div>
           <ul className="items">
-            {children.map((listItem, index) => (
-              <li className="item" key={`accordion-${index}`}>
-                <button type="button" className="toggle-header js-accordionToggle" aria-expanded="true" aria-controls={`section${index}`} id={`toggle${index}`} data-tabindex={index}>
-                  <span className="label">
-                    <div className="component content small-12 columns">
-                      <div className="component-content">
-                        <div className="field-heading" dangerouslySetInnerHTML={{ __html: listItem.fields.heading }} />
-                      </div>
-                    </div>
-                  </span>
-                </button>
-                <div className="toggle-content js-accordionContent" id={`section${index}`} aria-labelledby={`toggle${index}`} aria-hidden="false">
+            {children.map((listItem, index) => {
+              if (listItem.children && listItem.children.length > 0 && listItem.children[0].children && listItem.children[0].children[0].fields.youtubemovie) {
+                return (
+                  <Wrapper index={index} heading={listItem.fields.heading}>
+                    <Video renderingContext={{
+                      item: {
+                        fields: {
+                          youtubemovie: listItem.children[0].children[0].fields.youtubemovie
+                        }
+                      }
+                    }} />
+                  </Wrapper>
+                )
+              }
+
+              return (
+                <Wrapper index={index} heading={listItem.fields.heading}>
                   <div className="component content small-12 columns">
                     <div className="component-content">
                       <div className="field-content" dangerouslySetInnerHTML={{ __html: listItem.fields.content }} />
                     </div>
                   </div>
-                </div>
-              </li>
-            ))}
+                </Wrapper>
+              )
+            })}
           </ul>
         </div>
       </div>
