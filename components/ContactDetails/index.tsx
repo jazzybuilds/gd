02851dc7a079c523c0d2474f48febd98f93fad77
Marfat Abdullahi <marfat.abdullahi@akqa.com>
2021-01-04 11:25:34 +0000

    Using parameters to check variants in contactdetails and textbanner component

--- components/ContactDetails/index.tsx
+++ components/ContactDetails/index.tsx
@@ -1,18 +1,25 @@
 import React from 'react'
 import "../../styles/component-getintouch.scss";
 
+const Variants = {
+  All: "{55861F56-E0CD-43F3-96D9-574552AB7D10}",
+  EmailOnly: "{04CF5404-EC1F-4421-9216-20C86A0AEABB}",
+  PhoneOnly: "{FBBFC646-525D-4997-A37F-DD4262F5CB25}",
+}
+
 export const ContactDetails = (props) => {
   const { renderingContext } = props
+
   if (!renderingContext.item || !renderingContext.item.fields) {
+    console.error("No item supplied for ContactDetails")
     return null
   }
 
-  const { item: {id, fields } } = renderingContext
-  const datsource = renderingContext.page.renderings.find(rendering => rendering.settings.DataSource.includes(id.toUpperCase()))
+  const { item: {id, fields }, parameters } = renderingContext
 
-  const all = datsource?.settings.Parameters.includes("55861F56-E0CD-43F3-96D9-574552AB7D10") 
-  const emailOnly = datsource?.settings.Parameters.includes("04CF5404-EC1F-4421-9216-20C86A0AEABB") 
-  const phoneOnly = datsource?.settings.Parameters.includes("FBBFC646-525D-4997-A37F-DD4262F5CB25") 
+  const all = parameters?.FieldNames === Variants.All;
+  const emailOnly = parameters?.FieldNames === Variants.EmailOnly;
+  const phoneOnly = parameters?.FieldNames === Variants.PhoneOnly;
 
   return (
     <div className="component c-getInTouch small-12 columns">
