0498cb377ed938567e321023659f5b59bf56dc3a
Marfat Abdullahi <marfat.abdullahi@akqa.com>
2021-01-04 11:02:59 +0000

    Updated card component to read parameters

--- components/Card/index.tsx
+++ components/Card/index.tsx
@@ -1,22 +1,33 @@
 import React from 'react'
 import { Root, Wrapper, Content, Text, ImageWrapper, Image, Link } from "./Card.styles"
 
+const ManualNavigationPodVariants = {
+  Default: "{D288AE53-0DFF-418F-9773-52A51A40B582}",
+  NoImage: "{83D702CA-4797-4B98-9C9A-E861370CE490}",
+  Large: "{0199A687-14BF-4599-A99A-6A97576E18D8}"
+}
+
+const ManualNavigationPodStyles = {
+  MakeEqualHeight: "{3182392E-93CD-425C-8778-611B677FA58A}",
+  LightBlue: "{76720053-27F8-4CCF-8652-69A6A91FA586}",
+  DarkBlue: "{0D887421-9606-4A99-8B97-D20C79EAE2C0}",
+}
+
 export const Card = (props: any) => {
   const { renderingContext } = props
+
   if (!renderingContext.item || !renderingContext.item.fields) {
+    console.error("No item supplied for Card")
     return null
   }
 
-  const { item: { id, name, fields } } = renderingContext
+  const { item: { id, name, fields }, parameters } = renderingContext
 
-  const datsource = renderingContext.page.renderings.find(rendering => rendering.settings.DataSource.includes(id.toUpperCase()))
-
-  const isLarge = datsource?.settings.Parameters.includes('0199A687-14BF-4599-A99A-6A97576E18D8')
-  const noImage = datsource?.settings.Parameters.includes('83D702CA-4797-4B98-9C9A-E861370CE490')
-
-  const jsClass = datsource?.settings.Parameters.includes('3182392E-93CD-425C-8778-611B677FA58A')
-  const lightBlueBg = datsource?.settings.Parameters.includes('76720053-27F8-4CCF-8652-69A6A91FA586')
-  const darkBlueBg = datsource?.settings.Parameters.includes('0D887421-9606-4A99-8B97-D20C79EAE2C0')
+  const isLarge = parameters?.FieldNames === ManualNavigationPodVariants.Large;
+  const noImage = parameters?.FieldNames === ManualNavigationPodVariants.NoImage;
+  const jsClass = parameters?.Styles === ManualNavigationPodStyles.MakeEqualHeight;
+  const lightBlueBg = parameters?.Styles === ManualNavigationPodStyles.LightBlue;
+  const darkBlueBg = parameters?.Styles === ManualNavigationPodStyles.DarkBlue;
 
   let bgClass = "row-bg "
 
@@ -27,7 +38,6 @@ export const Card = (props: any) => {
   if (darkBlueBg) {
     bgClass += "row-bg--darkBlue"
   }
-
 
   let formattedUrl = fields.link?.url.split("/") ?? []
   if (formattedUrl.length > 0) {
