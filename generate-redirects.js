1f6b12a99182391d6fa6427e8a0732ab2fafb354
Alex Washtell <alex@kasaku.co.uk>
2021-01-12 15:43:44 +0000

    Sitecore proxies are now generated from the map.json file from Uniform.

--- generate-redirects.js
+++ generate-redirects.js
@@ -129,14 +129,48 @@ async function parseRedirects() {
   });
 }
 
+const sitecoreProxyRedirectTemplate = `[[redirects]]
+  from = '{SITECORE_PATH}'
+  to = '{SITECORE_ORIGIN}{SITECORE_PATH}'
+  status = 200
+  force = true
+  [redirects.headers]
+      Authorization = "Basic {SITECORE_PROXY_BASIC_AUTH}"		
+`;
+
+async function parseManagedExclusions() {
+  let mapUrl = `${UNIFORM_API_URL}/uniform/api/content/guidedogsdotorg/map.json`;
+
+  console.log(`Fetching map.json for exclusions from: ${mapUrl}`)
+
+  return fetch(mapUrl)
+    .then(res => res.json())
+    .then(data => {
+      return data.excluded.map(path => {
+        console.log("Creating proxy to Sitecore for " + path);
+        return sitecoreProxyRedirectTemplate.replace(/{SITECORE_PATH}/g, path)
+      }).join("\r\n");
+    })
+    .catch(error => {
+      // Could use environment variable here to define behaviour, e.g. throw error.
+      console.log("Error retrieving managed exclusions.");
+      console.log(error);
+
+      return "";
+    })
+}
+
 async function parseNetlifyToml() {
   console.log("Creating netlify.toml from netlify.toml.template");
+
+  const proxyRedirects = await parseManagedExclusions();
 
   fs.readFile('./netlify.toml.template', 'utf8', function (err, data) {
 
     let text = data.toString();
 
     // Update redirects in netlify.toml.template with origin hosts.
+    text = text.replace(/{PROXIES_TO_SITECORE}/g, proxyRedirects);
     
     text = text.replace(/{NETLIFY_URL}/g, process.env.URL);
     text = text.replace(/{SITECORE_ORIGIN}/g, process.env.SITECORE_ORIGIN);
