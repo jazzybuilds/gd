832237e85a4d9729db89375d6cabe6d29c3ad74a
Marfat Abdullahi <marfat.abdullahi@akqa.com>
2021-01-19 10:59:39 +0000

    Adding alt tag if content is available

--- components/Image/index.tsx
+++ components/Image/index.tsx
@@ -1,5 +1,15 @@
 import React from 'react'
 
+interface ImageProps {
+  src: string
+  width: string
+  height: string
+  "data-variantitemid": string
+  "data-variantfieldname": string
+  alt?: string
+  title?: string
+}
+
 export const Image = (props) => {
   const { renderingContext } = props
   if (!renderingContext.item || !renderingContext.item.fields || !renderingContext.item.fields.image) {
@@ -8,11 +18,27 @@ export const Image = (props) => {
 
   const { item: {id, fields: { imagecaption, image }}} = renderingContext
 
+  let imageAttr: ImageProps = {
+    src: `${image.url}?h=${image.height}&amp;w=${image.width}&amp;`,
+    width: image.width,
+    height: image.height,
+    "data-variantitemid": id,
+    "data-variantfieldname": "Image",
+  }
+
+  if (image.alt) {
+    imageAttr = {
+      ...imageAttr,
+      alt: image.alt,
+      title: image.alt,
+    }
+  }
+
   return (
     <div className="component c-imagePod file-type-icon-media-link small-12 columns">
       <div className="component-content">
         <div className="c-imagePod__image">
-          <img src={`${image.url}?h=${image.height}&amp;w=${image.width}&amp;`} alt={image.alt} width={image.width} height={image.height} data-variantitemid={id} data-variantfieldname="Image" title={image.alt} />
+          <img {...imageAttr} />
         </div>
         {imagecaption &&
           <div className="c-imagePod__content">
