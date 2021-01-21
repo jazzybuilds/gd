cd376e778ee9c854c0f586044c2e29aeeef4388d
Michael Carlisle <Michael.Carlisle@akqa.com>
2021-01-21 20:48:54 +0000

    Remove unused file.

--- api.webpack.config.js
+++
@@ -1,7 +0,0 @@
-/* eslint-disable */
-const { parsed: localEnv } = require('dotenv').config()
-const webpack = require('webpack')
-
-module.exports = {
-  plugins: [new webpack.EnvironmentPlugin(localEnv)],
-}
