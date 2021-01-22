23d65ea510d1fa7756f64d3248757e932b168055
Marfat Abdullahi <marfat.abdullahi@akqa.com>
2021-01-22 15:48:10 +0000

    Moved height calc out of react and into scripts file

--- public/scripts.js
+++ public/scripts.js
@@ -1,3 +1,5 @@
+const equalHeightPods=e=>{var t=document.querySelectorAll(".column-splitter");if(t)for(var r=0;r<t.length;r++){var o=t[r].querySelectorAll(e);o.length>1&&l(e,o)}function l(e,t){for(var r=t.length,o=[],l=0;l<r;l++){const e=t[l].querySelector('div[class*="__wrapper"]');e&&(e.style.minHeight="72px")}setTimeout(function(){window.innerWidth<640||function(){for(var e=0;e<r;e++)o.push(t[e].offsetHeight);for(var l=Math.max.apply(this,o),n=0;n<r;n++){const e=t[n].querySelector('div[class*="__wrapper"]');e&&(e.style.minHeight=l+"px")}}()},100)}};
+
 jQuery.noConflict()
 jQuery("a.js-responsiveNav-last.visuallyhidden").remove();
 
@@ -5,3 +7,14 @@ if (!document.cookie.includes("privacy-notification")) {
   jQuery(".privacy-warning").css("display", "block");
 }
 
+document.addEventListener('DOMContentLoaded', function() {
+  equalHeightPods('.js-equalHeight');
+})
+
+var resizeTimer;
+window.addEventListener('resize', function () {
+  clearTimeout(resizeTimer);
+  resizeTimer = setTimeout(function () {
+    equalHeightPods('.js-equalHeight');
+  }, 500);
+});
