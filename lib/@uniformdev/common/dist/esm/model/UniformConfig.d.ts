af3e659a7f51d57bc29ede9923ec6102ea2b8e26
ArtemNizelnyk <artem.nizelnik@gmail.com>
2021-02-17 18:27:42 +0200

    feat: add uniform_preview to querystring
    adds uniform_preview=true query string parameter when .env has UNIFORM_OPTIONS_PREVIEW=true

--- lib/@uniformdev/common/dist/esm/model/UniformConfig.d.ts
+++ lib/@uniformdev/common/dist/esm/model/UniformConfig.d.ts
@@ -4,6 +4,7 @@ export interface UniformConfig {
     UNIFORM_DATA_URL: string;
     UNIFORM_API_MAPSERVICE: string;
     UNIFORM_OPTIONS_DEBUG: boolean;
+    UNIFORM_OPTIONS_PREVIEW: boolean;
     UNIFORM_OPTIONS_PREFETCH_LINKS: boolean;
     UNIFORM_OPTIONS_MVC_SPA_ENABLED: boolean;
     UNIFORM_OPTIONS_ANALYTICS_ENABLED: boolean;
